a22da148b9c64cae0e9ddf8a7d7e5dca9cd1274a
Matthias Hammelehle <209659387+Matthias146@users.noreply.github.com>
2026-06-14 18:28:25 +0200

    feat(dashboard): integrate kpi cards and charts into dashboard

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { KpiCard } from '../../shared/components/kpi-card/kpi-card';
+import { NgIcon } from '@ng-icons/core';
+import { LineChart } from '../../shared/components/line-chart/line-chart';
+import { DonutChart } from '../../shared/components/donut-chart/donut-chart';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [KpiCard, NgIcon, LineChart, DonutChart],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.scss',
 })
